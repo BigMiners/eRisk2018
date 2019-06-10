aa764f0118d3b303b4de6e198b798e91d74d192a
Marc Queudot <queudot.marc@courrier.uqam.ca>
2019-06-10 11:23:04 -0400

    Remove folder of parser (dependency in requirements)

--- eRisk-2018-parser/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='erisk2018data',
-      version='0.3',
-      description='eRisk2018 data handler for both tasks',
-      license='MIT',
-      packages=['erisk2018data'],)
-
